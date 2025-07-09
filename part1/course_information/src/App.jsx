204b6f3b6355cc3d93906fe44380a5f618a22433
Jesper Edström <jesper.edstrom@aalto.fi>
2025-07-09 09:20:27 +0300

    Task 1.3 done

--- part1/course_information/src/App.jsx
+++ part1/course_information/src/App.jsx
@@ -1,4 +1,4 @@
-const App = () => {
+const App_step2 = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -12,6 +12,30 @@ const App = () => {
       <Header course={course} />
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total total={exercises1+exercises2+exercises3} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
+      <Total total={part1.exercises+part2.exercises+part3.exercises} />
     </div>
   )
 }

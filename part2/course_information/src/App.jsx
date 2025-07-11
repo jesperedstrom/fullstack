c373ded4b39ba9be9f2fe5331ebb176170dcdf3b
Jesper Edström <jesper.edstrom@aalto.fi>
2025-07-11 22:01:28 +0300

    Course information step 10 done

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -1,39 +1,4 @@
-const Header = (props) => <h2>{props.course}</h2>
-
-const Content = (props) => {
-  console.log(props)
-
-  return (
-    <div>
-      {props.parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => <p><b>total of {props.total} exercises</b></p>
-
-const Course = ({course}) => {
-
-  // Compute sum of exercises using reduce
-  const total = course.parts.map(part => part.exercises).reduce((sum, exercise) => sum + exercise, 0)
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={total}
-      />    
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [

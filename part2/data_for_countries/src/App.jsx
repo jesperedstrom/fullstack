1471bfcd5af1ad174542024567f56bff7e6355e6
Jesper Edström <jesper.edstrom@aalto.fi>
2025-07-16 10:52:09 +0300

    Data for countries step 3 done

--- part2/data_for_countries/src/App.jsx
+++ part2/data_for_countries/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const api_key = import.meta.env.VITE_SOME_KEY
+
 const Filter = ({searchText, onChange}) => (
   <form>
     <div>
@@ -9,15 +11,11 @@ const Filter = ({searchText, onChange}) => (
   </form>
 )
 
-const Countries = ({countries, onShow}) => {
+const CountriesList = ({countries, onShow}) => {
 
     if (countries.length > 10) {
       return(
         <div>Too many matches, specify another filter</div>
-      )
-    } else if (countries.length === 1) {
-      return (
-        <ShowSingleCountry country={countries[0]}/>
       )
     } else if (countries.length === 0) {
       return (
@@ -25,7 +23,7 @@ const Countries = ({countries, onShow}) => {
       )
     } else {
       return(
-      countries.map(country => <div key={country.cca3}>{country.name.common} <ShowButton onClick={()=>onShow([country])}/></div>)
+      countries.map(country => <div key={country.cca3}>{country.name.common} <ShowButton onClick={()=>onShow(country)}/></div>)
       )
     }
 }
@@ -52,6 +50,26 @@ const ShowSingleCountry = ({country}) => {
         src={country.flags.png}
         alt={country.flags.alt}
       />
+
+    </div>
+  )
+}
+
+const Weather =({weather, country}) => {
+
+  return (
+    <div>
+      <h2>Weather in {country.capital[0]}</h2>
+
+      Temperature {weather.temp} Celsius<br/>
+
+      <img 
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt={weather.weather[0].description}
+      /><br/>
+
+      Wind {weather.wind_speed} m/s
+
     </div>
   )
 }
@@ -60,6 +78,8 @@ function App() {
   const [searchText, setSearchText] = useState('')
   const [countryData, setCountryData] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
+  const [country, setCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
 
   const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
 
@@ -73,12 +93,41 @@ function App() {
     })
   }, [])
 
+  // Get weather data
+  useEffect(() => {
+    if (country) {
+      console.log(country)
+
+      const time = Math.floor(Date.now()/1000)
+      const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&units=metric&dt=${time}&appid=${api_key}`
+
+      axios
+      .get(weatherUrl)
+      .then(response => {
+        setWeather(response.data.data[0])
+        console.log(response.data.data[0])
+      })
+    }
+  }, [country])
+
   const handleSearchText = (event) => {
     setSearchText(event.target.value)
 
     // Filter country list
     const countries_filt = countryData.filter((country) => country.name.common.toLowerCase().indexOf(event.target.value.toLowerCase()) != -1)
     setFilteredCountries(countries_filt)
+
+    if (countries_filt.length === 1) {
+      // console.log(countries_filt[0].name.common)
+      setCountry(countries_filt[0])
+    } else {
+      setCountry(null)
+    }
+  }
+
+  const onShow = (country) => {
+    setFilteredCountries([country])
+    setCountry(country)
   }
 
   return (
@@ -86,7 +135,17 @@ function App() {
     <div>
       <Filter searchText={searchText} onChange={handleSearchText}/>
 
-      <Countries countries={filteredCountries} onShow={setFilteredCountries}/>
+      {(filteredCountries.length === 1) ? (
+        <div>
+          <ShowSingleCountry country={filteredCountries[0]}/>
+          {weather ? <Weather weather={weather} country={country}/> : 'Weather not retrieved'}
+        </div>
+        ) : (
+        <div>
+          <CountriesList countries={filteredCountries} onShow={onShow}/>
+        </div>
+      )}
+      
     </div>
 
     
